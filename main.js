8666dfba9c043afeb481f717bf9fa9814807beb9
Gautham C Sudheer <csgautham091003@gmail.com>
2024-03-16 12:33:05 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const countdownDate = new Date("2024-04-04T23:59:59").getTime();
+
+const countdown = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days.toString().padStart(2, '0');
+    document.getElementById("hours").innerText = hours.toString().padStart(2, '0');
+    document.getElementById("minutes").innerText = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").innerText = seconds.toString().padStart(2, '0');
+    
+    if (distance < 0) {
+        clearInterval(countdown);
+        document.getElementById("countdown").innerHTML = "Countdown expired";
+    }
+}, 1000);
